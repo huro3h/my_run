bd2a7653131c8dadfd0af970c66a48c4b70fa163
huro3h <huro3h@gmail.com>
2019-05-07 00:51:24 +0900

    Install gem sassc-rails instead of sass-rails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,6 +55,9 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
+  # for sassc-rails
+  config.sass.inline_source_maps = true
+
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
